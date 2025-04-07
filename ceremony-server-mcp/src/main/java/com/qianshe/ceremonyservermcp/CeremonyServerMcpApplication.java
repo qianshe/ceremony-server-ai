c261d4021479ad8b37700c5c4962d5578e497ebe
qianshe <cutoffother@gmail.com>
2025-04-07 20:01:41 +0800

    refactor(ceremony-server-mcp): 重构 MCP 服务器配置和依赖
    - 更新 application.yml 文件，添加服务器端口配置和 AI客户端配置
    - 修改 CeremonyServerMcpApplication 类，实现 CommandLineRunner 接口
    - 更新 mcp-servers.json 文件，配置标准 I/O MCP 服务器
    - 修改 pom.xml 文件，更新 MCP 客户端依赖版本

--- ceremony-server-mcp/src/main/java/com/qianshe/ceremonyservermcp/CeremonyServerMcpApplication.java
+++ ceremony-server-mcp/src/main/java/com/qianshe/ceremonyservermcp/CeremonyServerMcpApplication.java
@@ -1,13 +1,31 @@
 package com.qianshe.ceremonyservermcp;
 
+import jakarta.annotation.Resource;
+import org.springframework.ai.model.function.FunctionCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CeremonyServerMcpApplication {
+public class CeremonyServerMcpApplication implements CommandLineRunner {
+
+    @Resource
+    private ToolCallbackProvider toolCallbackProvider;
+
 
     public static void main(String[] args) {
         SpringApplication.run(CeremonyServerMcpApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) {
+        FunctionCallback[] toolCallbacks = toolCallbackProvider.getToolCallbacks();
+
+        System.out.println("ToolCallbackProvider: " +
+                Arrays.toString(toolCallbacks));
+    }
 }
